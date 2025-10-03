3e9c2e978d22e613a4934e4759291a69df20c75d
Miguel Santiago Barrera Castro <omungabarrera@gmail.com>
2025-10-03 14:29:01 -0500

    feat: habilita la edicion del perfil por el modal

--- scripts/index.js
+++ scripts/index.js
@@ -22,3 +22,43 @@ const initialCards = [
 initialCards.forEach(function(card) {
     console.log(card.name);
 });
+
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileEditModal = document.querySelector(`#edit-popup`);
+const closeProfileEditButton = profileEditModal.querySelector('.popup__close');
+
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const profileNameInput = profileEditModal.querySelector('.popup__input_type_name');
+const profileDescriptionInput = profileEditModal.querySelector('.popup__input_type_description');
+
+function openModal(modal) {
+    modal.classList.add(`popup_is-opened`);
+};
+function closeModal(modal) {
+    modal.classList.remove(`popup_is-opened`);
+};
+function fillProfileForm() {
+    profileNameInput.value = profileName.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+};
+function handleOpenEditModal() {
+    openModal(profileEditModal);
+    fillProfileForm();
+};
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = profileNameInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closeModal(profileEditModal);
+}
+
+profileEditButton.addEventListener('click', function() {
+    handleOpenEditModal();
+});
+
+closeProfileEditButton.addEventListener('click', function() {
+    closeModal(profileEditModal);
+});
+
+profileEditModal.addEventListener(`submit`, handleProfileFormSubmit);
