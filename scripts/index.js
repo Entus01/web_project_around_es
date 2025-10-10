1fb6b737757440391af3d4784c6773c0f5d27e12
Miguel Santiago Barrera Castro <omungabarrera@gmail.com>
2025-10-10 18:54:25 -0500

    agrega el codigo de JS para agregar tarjetas, darles like y borrarlas

--- scripts/index.js
+++ scripts/index.js
@@ -19,10 +19,6 @@ const initialCards = [
     }
 ];
 
-initialCards.forEach(function(card) {
-    console.log(card.name);
-});
-
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileEditModal = document.querySelector(`#edit-popup`);
 const closeProfileEditButton = profileEditModal.querySelector('.popup__close');
@@ -31,6 +27,12 @@ const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 const profileNameInput = profileEditModal.querySelector('.popup__input_type_name');
 const profileDescriptionInput = profileEditModal.querySelector('.popup__input_type_description');
+
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
+const cardsContainer = document.querySelector('.cards__list');
+const newCardBtn = document.querySelector('.profile__add-button');
+const newCardModal = document.querySelector('#new-card-popup');
+const closeNewCardBtn = newCardModal.querySelector('.popup__close');
 
 function openModal(modal) {
     modal.classList.add(`popup_is-opened`);
@@ -53,6 +55,71 @@ function handleProfileFormSubmit(evt) {
     closeModal(profileEditModal);
 }
 
+function getCardElement(card) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+
+    console.log(cardElement);
+    console.log(cardTitle);
+
+    cardTitle.textContent = card.name;
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+
+    if (cardTitle.textContent === '') {
+        cardTitle.textContent = 'Sin título';
+    };
+    if (cardImage.src === '') {
+        cardImage.src = '../images/placeholder.jpg';
+        cardImage.alt = 'Imagen de marcador de posición';
+    };
+
+    const likeBtn = cardElement.querySelector('.card__like-button');
+    likeBtn.addEventListener('click', function() {
+        likeBtn.classList.toggle('card__like-button_is-active');
+    });
+
+    const deleteBtn = cardElement.querySelector('.card__delete-button');
+    deleteBtn.addEventListener('click', function(){
+        cardElement.remove();
+    })
+
+    cardImage.addEventListener('click', function() {
+        const imageModal = document.querySelector('#image-popup');
+        const imageModalImg = imageModal.querySelector('.popup__image');
+        const imageModalCaption = imageModal.querySelector('.popup__caption');
+
+        imageModalImg.src = card.link;
+        imageModalImg.alt = card.name;
+        imageModalCaption.textContent = card.name;
+
+        openModal(imageModal);
+
+        const closeImageModalBtn = imageModal.querySelector('.popup__close');
+        closeImageModalBtn.addEventListener('click', function() {
+            closeModal(imageModal);
+        });
+    })
+
+    return cardElement;
+}
+
+function renderCard(name, link, container) {
+    const card = {name, link};
+    const cardElement = getCardElement(card);
+    container.prepend(cardElement);
+}
+
+function handleCardFormSubmit(evt) {
+    evt.preventDefault();
+    const cardNameUInput = newCardModal.querySelector('.popup__input_type_card-name');
+    const cardLinkInput = newCardModal.querySelector('.popup__input_type_url');
+    renderCard(cardNameUInput.value, cardLinkInput.value, cardsContainer);
+    evt.target.reset();
+    closeModal(newCardModal);
+}
+
 profileEditButton.addEventListener('click', function() {
     handleOpenEditModal();
 });
@@ -62,3 +129,17 @@ closeProfileEditButton.addEventListener('click', function() {
 });
 
 profileEditModal.addEventListener(`submit`, handleProfileFormSubmit);
+
+initialCards.forEach(function(card) {
+    renderCard(card.name, card.link, document.querySelector('.cards__list'));
+});
+
+newCardBtn.addEventListener('click', function() {
+    openModal(newCardModal);
+});
+
+closeNewCardBtn.addEventListener('click', function() {
+    closeModal(newCardModal);
+});
+
+newCardModal.addEventListener('submit', handleCardFormSubmit);
