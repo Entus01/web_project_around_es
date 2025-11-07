80bc959aa21edeb91a6b247968658a5862a4cea9
Miguel Santiago <omungabarrera@gmail.com>
2025-11-07 14:55:36 -0500

    corregir la nomenclatura de los archivos FormValidator y Card

--- scripts/formValidator.js
+++ scripts/formValidator.js
@@ -1,1 +1,56 @@
 /*Importar recursos de index.js, card.js y utils.js*/
+
+export class FormValidator {
+  constructor(element, selector) {
+    this._element = element;
+    this.selector = selector;
+  };
+
+  setEventListeners() {
+    this._inputList = this._element.querySelectorAll(this.selector.input);
+    this._buttonElement = this._element.querySelector(this.selector.submitButton);
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        if (!inputElement.validity.valid) {
+          this._showInputError(inputElement, inputElement.validationMessage);
+        } else {
+          this._hideInputError(inputElement);
+        }
+        this._toggleButtonState();
+      });
+    });
+  };
+
+  _toggleButtonState() {
+    if (this._isFormValid()) {
+      this._buttonElement.disabled = false;
+    } else {
+      this._buttonElement.disabled = true;
+    }
+  };
+
+  _isFormValid() {
+    return Array.from(this._inputList).every(
+      (inputElement) => inputElement.validity.valid
+    );
+  };
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = document.querySelector(`.${inputElement.id}-input-error`);
+    inputElement.classList.add(`popup__input_type_error`);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(`popup__input-error_active`);
+  };
+
+  _hideInputError(inputElement) {
+    const errorElement = document.querySelector(
+      `.${inputElement.id}-input-error`
+    );
+    inputElement.classList.remove(`popup__input_type_error`);
+    errorElement.textContent = "";
+    errorElement.classList.remove(`popup__input-error_active`);
+  };
+}
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import { openModal, closeModal } from "./utils.js";
-import { Card } from "./card.js";
+import { Card } from "./Card.js";
+import { FormValidator } from "./FormValidator.js";
 
 const initialCards = [
   {
@@ -88,62 +89,6 @@ function handleCardFormSubmit(evt) {
   renderCard(cardNameUInput.value, cardLinkInput.value, cardsContainer);
   evt.target.reset();
   closeModal(newCardModal);
-}
-
-class FormValidator {
-  constructor(element, selector) {
-    this._element = element;
-    this.selector = selector;
-  };
-
-  setEventListeners() {
-    this._inputList = this._element.querySelectorAll(this.selector.input);
-    this._buttonElement = this._element.querySelector(this.selector.submitButton);
-    this._toggleButtonState();
-
-    console.log(this._inputList);
-
-    this._inputList.forEach((inputElement) => {
-      inputElement.addEventListener("input", () => {
-        if (!inputElement.validity.valid) {
-          this._showInputError(inputElement, inputElement.validationMessage);
-        } else {
-          this._hideInputError(inputElement);
-        }
-        this._toggleButtonState();
-      });
-    });
-  };
-
-  _toggleButtonState() {
-    if (this._isFormValid()) {
-      this._buttonElement.disabled = false;
-    } else {
-      this._buttonElement.disabled = true;
-    }
-  };
-
-  _isFormValid() {
-    return Array.from(this._inputList).every(
-      (inputElement) => inputElement.validity.valid
-    );
-  };
-
-  _showInputError(inputElement, errorMessage) {
-    const errorElement = document.querySelector(`.${inputElement.id}-input-error`);
-    inputElement.classList.add(`popup__input_type_error`);
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add(`popup__input-error_active`);
-  };
-
-  _hideInputError(inputElement) {
-    const errorElement = document.querySelector(
-      `.${inputElement.id}-input-error`
-    );
-    inputElement.classList.remove(`popup__input_type_error`);
-    errorElement.textContent = "";
-    errorElement.classList.remove(`popup__input-error_active`);
-  };
 }
 
 const profileFormValidator = new FormValidator(profileForm, {
